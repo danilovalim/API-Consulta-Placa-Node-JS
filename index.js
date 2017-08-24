20875342f2cc79c75ed103af5d62616b12fbc5cf
danilovalim <danilovalim@hotmail.com>
2017-08-24 12:06:36 -0300

    add GET method

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var express = require('express');
+sinesp = require('sinesp-nodejs');
 
 var app = express();
+
+app.get('/:value', (req, res)=> {
+    sinesp.consultaPlaca(req.params.value, (response)=>{
+        res.send(response);
+    })
+})
 
 app.listen(3000);
